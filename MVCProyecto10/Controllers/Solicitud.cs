94df70b2d78270f768f524806eb45a7bd9031059
Luis Fernando <fer744@gmail.com>
2020-10-17 00:05:39 -0600

    Se agrega la vista de Solicitud

---
+++ MVCProyecto10/Controllers/Solicitud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProyecto10.Controllers
+{
+    public class Solicitud : Controller
+    {
+        public Solicitud()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View("Solicitud");
+        }
+    }
+}
